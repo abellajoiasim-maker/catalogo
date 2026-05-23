e6796ae5ab3a4250ccdba148ed5b59a66b85cb03
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-05-22 21:01:20 -0300

    Update firebase.js

--- images/js/firebase/firebase.js
+++ images/js/firebase/firebase.js
@@ -1,15 +1,25 @@
-// FILE: /js/firebase/firebase.js
-
-// Configuração estrita do Firebase Realtime Database para Abella Joias
+// CONFIGURAÇÃO CENTRAL E INICIALIZAÇÃO DO FIREBASE COLETIVO
 const firebaseConfig = {
     apiKey: "AIzaSyDPBZSxW8XjtQmDMUknzAyIlFda51MvMJY",
-    databaseURL: "https://catalogo-abella-joias-default-rtdb.firebaseio.com"
+    authDomain: "catalogo-abella-joias.firebaseapp.com",
+    databaseURL: "https://catalogo-abella-joias-default-rtdb.firebaseio.com",
+    projectId: "catalogo-abella-joias",
+    storageBucket: "catalogo-abella-joias.firebasestorage.app",
+    messagingSenderId: "727568435294",
+    appId: "1:727568435294:web:442c0179ecf0686dff4ccf"
 };
 
-// Inicialização segura e isolada sem duplicação de instâncias
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+// Verifica se o SDK tradicional (compat) está presente na janela global
+if (typeof window.firebase !== "undefined") {
+    if (!window.firebase.apps.length) {
+        window.firebase.initializeApp(firebaseConfig);
+    }
+    // Garante que db esteja disponível globalmente para códigos soltos nos HTMLs
+    if (!window.db) {
+        window.db = window.firebase.database();
+    }
 }
 
-// Exportação global limpa da instância do banco de dados (Sem tags <script> internas)
-const db = firebase.database();
+// Exporta para os serviços modulares usarem via importação ES6
+const db = window.db || (typeof window.firebase !== "undefined" ? window.firebase.database() : null);
+export { db, firebaseConfig };

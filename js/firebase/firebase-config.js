d66871a4db485dd60a7a84e15a242e7b09ad14fa
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-05-28 20:52:32 -0300

    Update firebase-config.js

--- js/firebase/firebase-config.js
+++ js/firebase/firebase-config.js
@@ -1,1 +1,16 @@
+// Configuração do Firebase Realtime Database da Abella Joias
+const firebaseConfig = {
+    apiKey: "AIzaSy...",
+    authDomain: "abella-joias.firebaseapp.com",
+    databaseURL: "https://abella-joias-default-rtdb.firebaseio.com",
+    projectId: "abella-joias",
+    storageBucket: "abella-joias.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:123456:web:abc123xyz"
+};
 
+// Inicializa o Firebase globalmente
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const database = firebase.database();

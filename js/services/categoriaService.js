2bd05db587df81e6e9194be717973cae885b9d61
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-05-28 20:56:41 -0300

    Update categoriaService.js

--- js/services/categoriaService.js
+++ js/services/categoriaService.js
@@ -1,1 +1,11 @@
-
+const CategoriaService = {
+    async obterTodas() {
+        return new Promise((resolve) => {
+            database.ref('categorias').once('value', (snapshot) => {
+                const dados = snapshot.val();
+                const lista = dados ? Object.keys(dados).map(key => ({ id: key, ...dados[key] })) : [];
+                resolve(lista);
+            });
+        });
+    }
+};

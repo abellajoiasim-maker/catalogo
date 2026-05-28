15cd8967555e9023851b19d210c2386492352714
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-05-28 20:56:14 -0300

    Update produtoService.js

--- js/services/produtoService.js
+++ js/services/produtoService.js
@@ -1,1 +1,19 @@
+const ProdutoService = {
+    async obterTodos() {
+        return new Promise((resolve) => {
+            database.ref('produtos').once('value', (snapshot) => {
+                const dados = snapshot.val();
+                const lista = dados ? Object.keys(dados).map(key => ({ id: key, ...dados[key] })) : [];
+                resolve(lista);
+            });
+        });
+    },
 
+    async obterPorId(id) {
+        return new Promise((resolve) => {
+            database.ref(`produtos/${id}`).once('value', (snapshot) => {
+                resolve(snapshot.val());
+            });
+        });
+    }
+};

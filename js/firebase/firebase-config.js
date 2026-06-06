669f0c1d32a606378fe1f98e419e7047d9b0e873
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-06-05 21:09:58 -0300

    Simplify firebaseConfig declaration

--- js/firebase/firebase-config.js
+++ js/firebase/firebase-config.js
@@ -39,30 +39,15 @@
     // ==========================================================
     // CONFIGURAÇÃO OFICIAL FIREBASE
     // ==========================================================
-    const firebaseConfig = Object.freeze({
-
-        apiKey:
-            'AIzaSyDPBZSxW8XjtQmDMUknzAyIlFda51MvMJY',
-
-        authDomain:
-            'catalogo-abella-joias.firebaseapp.com',
-
-        databaseURL:
-            'https://catalogo-abella-joias-default-rtdb.firebaseio.com',
-
-        projectId:
-            'catalogo-abella-joias',
-
-        storageBucket:
-            "catalogo-abella-joias.firebasestorage.app"
-
-        messagingSenderId:
-            '727568435294',
-
-        appId:
-            '1:727568435294:web:442c0179ecf0686dff4ccf'
-
-    });
+   const firebaseConfig = Object.freeze({
+    apiKey: 'AIzaSyDPBZSxW8XjtQmDMUknzAyIlFda51MvMJY',
+    authDomain: 'catalogo-abella-joias.firebaseapp.com',
+    databaseURL: 'https://catalogo-abella-joias-default-rtdb.firebaseio.com',
+    projectId: 'catalogo-abella-joias',
+    storageBucket: 'catalogo-abella-joias.firebasestorage.app',
+    messagingSenderId: '727568435294',
+    appId: '1:727568435294:web:442c0179ecf0686dff4ccf'
+});
 
     // ==========================================================
     // INICIALIZAÇÃO SEGURA

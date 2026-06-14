acb2ea116319f8ad3ac1914dedf3ed5cf0c7911b
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-06-14 07:35:05 -0300

    Refactor peso.js for improved weight formatting

--- js/utils/peso.js
+++ js/utils/peso.js
@@ -1,8 +1,39 @@
-window.formatarPeso = function(valor){
+/**
+ * =====================================================
+ * PESO UTILS v1.0
+ * Abella Joias
+ * =====================================================
+ */
 
-    valor = Number(valor || 0);
+(function () {
 
-    if(valor >= 1000){
+    /**
+     * Formata peso automaticamente:
+     *
+     * 999.99   => 999,99 g
+     * 1000     => 1,00 kg
+     * 1250     => 1,25 kg
+     * 99999.99 => 100,00 kg
+     */
+
+    function formatarPeso(valor) {
+
+        valor = Number(valor || 0);
+
+        if (isNaN(valor)) {
+            valor = 0;
+        }
+
+        if (valor >= 1000) {
+
+            return new Intl.NumberFormat(
+                'pt-BR',
+                {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2
+                }
+            ).format(valor / 1000) + ' kg';
+        }
 
         return new Intl.NumberFormat(
             'pt-BR',
@@ -10,14 +41,49 @@ window.formatarPeso = function(valor){
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2
             }
-        ).format(valor / 1000) + ' kg';
+        ).format(valor) + ' g';
     }
 
-    return new Intl.NumberFormat(
-        'pt-BR',
-        {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
+    /**
+     * Retorna peso somente numérico
+     * Ex:
+     * 1250 => 1,25
+     */
+
+    function pesoNumero(valor) {
+
+        valor = Number(valor || 0);
+
+        if (valor >= 1000) {
+            return valor / 1000;
         }
-    ).format(valor) + ' g';
-};
+
+        return valor;
+    }
+
+    /**
+     * Retorna unidade automaticamente
+     */
+
+    function unidadePeso(valor) {
+
+        valor = Number(valor || 0);
+
+        return valor >= 1000
+            ? 'kg'
+            : 'g';
+    }
+
+    /**
+     * Disponibiliza globalmente
+     */
+
+    window.formatarPeso = formatarPeso;
+    window.pesoNumero = pesoNumero;
+    window.unidadePeso = unidadePeso;
+
+    console.log(
+        '⚖️ PesoUtils v1.0 carregado.'
+    );
+
+})();

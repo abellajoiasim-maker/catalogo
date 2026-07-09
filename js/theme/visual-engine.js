29e2961bb05a11ed439f3f44440c9444f4245e3e
abellajoiasim-maker <abellajoiasim@gmail.com>
2026-07-09 11:38:31 -0300

    Implement VisualEngine class with initialization methods

---
+++ js/theme/visual-engine.js
@@ -0,0 +1,33 @@
+class VisualEngine{
+
+    init(){
+
+        this.applyBodyClass();
+
+        this.enableAnimations();
+
+        this.observeCards();
+
+        this.observeImages();
+
+    }
+
+    applyBodyClass(){
+
+    }
+
+    enableAnimations(){
+
+    }
+
+    observeCards(){
+
+    }
+
+    observeImages(){
+
+    }
+
+}
+
+window.VisualEngine = new VisualEngine();
